8cb38cf3fc4a030bfad7bdccae660d564a5667b0
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-11-12 19:04:21 +0300

    Add project files.

---
+++ BaseProject.Identity/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using BaseProject.Identity.Entities;
+using BaseProject.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProject.Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user=await _userManager.FindByEmailAsync(model.Email);
+            if (user is null) return View(model);
+
+            var signInResult = await _signInManager.PasswordSignInAsync(user,model.Password,true,false);
+
+            if (signInResult.Succeeded) return RedirectToAction(nameof(HomeController.Index), "Home");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ BaseProject.Identity/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BaseProject.Identity.Entities
+{
+    public class AppUser:IdentityUser
+    {
+    }
+}
