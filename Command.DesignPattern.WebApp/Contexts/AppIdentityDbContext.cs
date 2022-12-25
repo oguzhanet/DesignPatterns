9cf31bc929d91a74e3c1ecd343349ea80934b1e5
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-12-25 15:53:36 +0300

    25.12.2022

---
+++ Command.DesignPattern.WebApp/Contexts/AppIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BaseProject.Identity.Entities;
+using Command.DesignPattern.WebApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Identity.Contexts
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Command.DesignPattern.WebApp/Migrations/20221225122313_add-product.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseProject.Identity.Migrations
+{
+    public partial class addproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Stock = table.Column<short>(type: "smallint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
